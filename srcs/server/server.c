4509fa2d75e608219c815c26a984377f8acd3af8
Mathias MAURAISIN <mathias.mrsn@MacBook-Pro-de-Mathias.local>
2021-11-14 16:25:12 +0100

    reception and display messages

--- srcs/server/server.c
+++ srcs/server/server.c
@@ -1,18 +1,52 @@
 #include "../../includes/minitalk.h"
 
-void	print(int signal)
+int	ft_atoi_binary(char c[9])
 {
-	if (signal == SIGUSR1)
-		ft_printf("0");
-	else
-		ft_printf("1");
+	int	i;
+	int	res;
+
+	res = 0;
+	i = 0;
+	while(i < 8)
+	{
+		res *= 2;
+		res += c[i] - 48;
+		i++;
+	}
+	return (res);
+}
+
+
+void	ft_stock_char(int signal)
+{
+	static char c[9];
+	char character;
+	static int i = 0;
+
+	if(i < 8)
+	{
+		if(signal == SIGUSR1)
+			c[i] = '0';
+		else
+			c[i] = '1';
+		i++;
+	}
+	if(i == 8)
+	{
+		c[8] = '\0';
+		character = ft_atoi_binary(c);
+		if(character == 0)
+			ft_printf("\n");
+		ft_printf("%c", character);
+		i = 0;
+	}
 }
 
 int	main(void)
 {
 	printf("PID : %d\n", getpid());
-	signal(SIGUSR1, &print);
-	signal(SIGUSR2, &print);
+	signal(SIGUSR1, &ft_stock_char);
+	signal(SIGUSR2, &ft_stock_char);
 	while(1)
 		pause();
 	return (0);
