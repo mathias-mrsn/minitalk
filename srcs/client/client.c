ff84450f5056bd21d594969ac6d64b2bb86d9d2e
Mathias MAURAISIN <mathias.mrsn@MacBook-Pro-de-Mathias.local>
2021-11-14 13:53:36 +0100

    client ready.

--- srcs/client/client.c
+++ srcs/client/client.c
@@ -1,6 +1,49 @@
 #include "../../includes/minitalk.h"
 
-int	main(void)
+void	ft_char_to_binary(int pid, const char c)
 {
-	printf("CLIENT");
+	int	index;
+
+	index = 7;
+	while(index > -1)
+	{
+		if(1 & c >> index)
+		{
+			if(kill(pid, SIGUSR2) == -1)
+				exit(0);
+		}
+		else
+		{
+			if(kill(pid, SIGUSR1) == -1)
+				exit(0);
+		}
+		usleep(50);
+		index--;
+	}
+}
+
+void	ft_encoding_str(int pid, char *str)
+{
+	int	index;
+
+	index = 0;
+	while(str[index])
+	{
+		ft_char_to_binary(pid, str[index]);
+		index++;
+	}
+	ft_char_to_binary(pid, '\0');
+}
+
+int	main(int argc, char **argv)
+{
+	int	pid;
+
+	if (argc < 3)
+		ft_printf("minitalk : arguments missing");
+	if (argc > 3)
+		ft_printf("minitalk : too many arguments");
+	pid = ft_atoi(argv[1]);
+	ft_encoding_str(pid, argv[2]);
+	return(0);
 }
--- srcs/server/server.c
+++ srcs/server/server.c
@@ -1,6 +1,41 @@
 #include "../../includes/minitalk.h"
 
+void	print(int signal)
+{
+	if (signal == SIGUSR1)
+		ft_printf("0");
+	else
+		ft_printf("1");
+}
+
 int	main(void)
 {
-	printf("SERVER");
+	printf("PID : %d\n", getpid());
+	signal(SIGUSR1, &print);
+	signal(SIGUSR2, &print);
+	while(1)
+		pause();
+	return (0);
 }
+
+/*----------------------------------------------------------------
+
+
+signal() 	: Get a signal and execute a function with in param his signal (signal, function)
+
+kill()		: Sent a signal to a pid (pid, signal)
+
+getpid()	: Get the pid of the server program
+
+pause()		: Suspends the program until a singal arrives (void)
+
+usleep()	: Suspends the program during n microseconds (useconds_t)
+
+exit()		: Exit of the program
+
+sigaction()	: 
+
+
+SIGUSR 1, 2 :
+
+*/
