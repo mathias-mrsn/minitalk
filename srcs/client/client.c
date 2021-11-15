074df6c971d141c91ecc23dd3697e8c36a8c9c30
Mathias MAURAISIN <mathias.mrsn@MacBook-Pro-de-Mathias.local>
2021-11-15 12:53:07 +0100

    fully fonctional + gnl

--- srcs/client/client.c
+++ srcs/client/client.c
@@ -17,7 +17,7 @@ void	ft_char_to_binary(int pid, const char c)
 			if(kill(pid, SIGUSR1) == -1)
 				exit(0);
 		}
-		usleep(50);
+		usleep(100);
 		index--;
 	}
 }
@@ -35,15 +35,31 @@ void	ft_encoding_str(int pid, char *str)
 	ft_char_to_binary(pid, '\0');
 }
 
+void	ft_stdin(int pid)
+{
+	char *str;
+
+	str = get_next_line(0);
+	while(str)
+	{
+		ft_encoding_str(pid, str);
+		str = get_next_line(0);
+	}
+}
+
+
+
 int	main(int argc, char **argv)
 {
 	int	pid;
 
-	if (argc < 3)
+	if (argc < 2)
 		ft_printf("minitalk : arguments missing");
 	if (argc > 3)
 		ft_printf("minitalk : too many arguments");
 	pid = ft_atoi(argv[1]);
+	if(argc == 2)
+		ft_stdin(pid);
 	ft_encoding_str(pid, argv[2]);
 	return(0);
 }
--- srcs/server/server.c
+++ srcs/server/server.c
@@ -17,12 +17,20 @@ int	ft_atoi_binary(char c[9])
 }
 
 
-void	ft_stock_char(int signal)
+void	ft_stock_char(int signal, siginfo_t *siginfo, void *data)
 {
 	static char c[9];
+	static int	start = 0;
 	char character;
 	static int i = 0;
 
+	if(i == 0)
+		ft_memset(c, '\0', 9);
+	if(start == 0)
+	{
+		start = 1;
+		ft_printf("%d --> ", siginfo->si_pid);
+	}
 	if(i < 8)
 	{
 		if(signal == SIGUSR1)
@@ -35,20 +43,31 @@ void	ft_stock_char(int signal)
 	{
 		c[8] = '\0';
 		character = ft_atoi_binary(c);
-		if(character == 0)
-			ft_printf("\n");
 		ft_printf("%c", character);
+		if(character == 0)
+		{
+			ft_printf("\n");
+			start = 0;
+		}
 		i = 0;
 	}
 }
 
 int	main(void)
 {
-	printf("PID : %d\n", getpid());
-	signal(SIGUSR1, &ft_stock_char);
-	signal(SIGUSR2, &ft_stock_char);
+	struct sigaction sa;
+
+	ft_memset(&sa, 0, sizeof(struct sigaction));
+	sa.sa_sigaction = ft_stock_char;
+	sa.sa_flags = SA_SIGINFO | SA_RESTART;
+
+
+	printf("|------------PID : %d------------|\n", getpid());
+	sigaction(SIGUSR1, &sa, NULL);
+	sigaction(SIGUSR2, &sa, NULL);
 	while(1)
 		pause();
+	printf("END");
 	return (0);
 }
 
